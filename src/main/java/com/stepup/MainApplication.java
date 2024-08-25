a10ecb5517c44f217a2834c2d03afd3b7098d531
Mayya Levina <maya-levina@yandex.ru>
2024-08-25 13:02:03 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -4,7 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        System.out.println("1111");
+        System.out.println("111111");
 
     }
 }
