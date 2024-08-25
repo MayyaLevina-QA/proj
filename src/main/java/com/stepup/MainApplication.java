742ae20ca73133ec0c9037f88ebb6219f7e7a511
Mayya Levina <maya-levina@yandex.ru>
2024-08-25 12:51:32 +0300

    initial commit2

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -3,7 +3,8 @@ package com.stepup;
 public class MainApplication {
     public static void main(String[] args) {
 
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("1111");
 
     }
 }
